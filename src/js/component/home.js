81c50aaf8606e6be5ff666aa8f879565273c3e8c
kevin-soria <Kevin.soria@outlook.com>
2020-02-24 14:52:11 +0000

    qa:! quit :wq

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,18 +1,51 @@
-import React from "react";
+// import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-
-// <-------------------------^components^------------------------->
 export function Home() {
-	return;
-	<div className="container" className="text-center mt-5">
-		<div className="redLight" />
-		{/* <-------------------end of red light-----------------------> */}
-	</div>;
-	{
-		/* <---------end of light fixture----------> */
-	}
+	const [redLight, setRedlight] = useState("inactive");
+	const [yellowLight, setYellowlight] = useState("inactive");
+	const [greenLight, setGreenlight] = useState("inactive");
+
+	return (
+		<div className="container lightFixture">
+			<div
+				className={redLight}
+				onClick={() => {
+					setRedlight(redLight === "red" ? "inactive" : "red");
+					setYellowlight(
+						redLight === "red" ? "inactive" : "inactive"
+					);
+					setGreenlight(redLight === "red" ? "inactive" : "inactive");
+				}}
+			/>
+			<div
+				className={yellowLight}
+				onClick={() => {
+					setYellowlight(
+						yellowLight === "yellow" ? "inactive" : "yellow"
+					);
+					setRedlight(
+						yellowLight === "red" ? "inactive" : "inactive"
+					);
+					setGreenlight(
+						yellowLight === "yellow" ? "inactive" : "inactive"
+					);
+				}}
+			/>
+			<div
+				className={greenLight}
+				onClick={() => {
+					setGreenlight(
+						greenLight === "green" ? "inactive" : "green"
+					);
+					setRedlight(
+						greenLight === "green" ? "inactive" : "inactive"
+					);
+					setYellowlight(
+						greenLight === "green" ? "inactive" : "inactive"
+					);
+				}}
+			/>
+		</div>
+	);
 }
